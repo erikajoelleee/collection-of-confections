8bc225388f7d775b1fa0525aee9db6a4976007fd
Erika Roncal-Schuyler <Roncal.schuyler@gmail.com>
2023-07-21 15:10:05 -0400

    passport

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('dotenv').config();
+var session = require('express-session');
+var passport = require('passport');
 var methodOverride = require('method-override');
+
+require('dotenv').config();
+require('./config/database');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +25,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
